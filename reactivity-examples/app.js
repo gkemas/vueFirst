6942635c560c82a242e09141603eb9c2c75dcf02
gkemas <kemasgokhan@gmail.com>
2024-04-17 17:13:12 +0300

    reactivity

---
+++ reactivity-examples/app.js
@@ -0,0 +1,20 @@
+const app = Vue.createApp({
+   data(){
+    return{
+        search: "",
+        itemList : ["elma","armut","kiraz","çilek"],
+        // filteredList :[],
+    };
+   },
+   methods:{
+    searchlist(){
+    //    this.filteredList = this.itemList.filter(i => i.includes(this.search));
+    //     console.log(filteredList);
+    },
+   },
+   computed:{
+    filteredList(){
+      return  this.itemList.filter(i => i.includes(this.search));
+    }
+   }
+}).mount('#app');
